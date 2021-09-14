d02e844ffba76ab3804b8190f1eb584ab90c21a7
TizianoVera <tiziano.vera90@gmail.com>
2021-09-14 12:13:36 -0300

    actualicação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./styles.css";
+import "./assets/styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Loja</h1>
       <h2>Pernanbucas</h2>
+      <div>
+        <nav>
+        <ul>
+          <li><a href="#">Produtos</a></li>
+          <li><a href="#">compras</a></li>
+          <li><a href="#">shop</a></li>
+        </ul>
+        </nav>
+      </div>
     </div>
   );
 }
