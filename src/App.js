c5d38f024533e7afe381d35cb08090e85c039f86
jhonmanuelg <jhonmanuelg6@outlook.com>
2021-09-20 17:04:22 -0300

    Subendo actualisação do projeto 2.0

--- src/App.js
+++ src/App.js
@@ -1,85 +1,23 @@
-import "./assets/styles.css";
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button, } from "react-bootstrap"
-import { Carousel, } from "react-bootstrap"
+import React, { Component } from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 
-export default function App() {
-  return (
-    <div className="App">
+import Header from "./components/header/Header";
+import CardBody from './components/body/CardBody.js';
 
-      <Navbar bg="primary" expand="lg">
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="mr-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex justify-content-flex-end">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="mr-2"
-              aria-label="Search"
-            />
-            <Button variant="info">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
 
-      // Inicio do Scroll //
-      <Carousel fade>
-        <Carousel.Item>
-          <img
-            className="vtex-carousel-2-x-img vtex-carousel-2-x-img--mx w-75 h-75 fz-100"
-            src="https://cdn.pixabay.com/photo/2017/04/23/09/44/smoothies-2253423__340.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="vtex-carousel-2-x-img vtex-carousel-2-x-img--mx w-75 h-75"
-            src="https://cdn.pixabay.com/photo/2016/10/09/14/00/vegetable-juices-1725835__340.jpg"
-            alt="Second slide"
-          />
+class App extends Component {
+  render() {
 
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="vtex-carousel-2-x-img vtex-carousel-2-x-img--mx w-75 h-75"
-            src="https://cdn.pixabay.com/photo/2017/01/22/19/12/pizza-2000595__340.jpg"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
-
-  );
+    return(
+      <div className="App">
+           <Header />
+           <CardBody />
+      </div>
+    );
+    
+  }
 }
+
+export default App;
---
+++ src/components/body/CardBody.js
@@ -0,0 +1,142 @@
+import React from "react";
+import { CardGroup, Card } from "react-bootstrap";
+
+const CardBody = () => {
+  return (
+    <div className=" comtainer">
+      <CardGroup classNome=" row row-cols-2" >
+        <Card className="" >
+          <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/03/26/10/58/apple-692186__340.jpg" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/09/04/23/28/wordpress-923188__480.jpg" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762__340.jpg" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      <hr/>
+      <CardGroup className="row row-cols-2">
+        <Card className="">
+          <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/03/26/10/58/apple-692186__340.jpg" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/09/04/23/28/wordpress-923188__480.jpg" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762__340.jpg" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      <hr/>
+      <CardGroup className="row row-cols-2">
+        <Card className="">
+          <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/03/26/10/58/apple-692186__340.jpg" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/09/04/23/28/wordpress-923188__480.jpg" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762__340.jpg" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+    </div>
+  );
+};
+
+export default CardBody;
